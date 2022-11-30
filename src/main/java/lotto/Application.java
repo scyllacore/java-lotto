523f863affc794f04e6b99e7a99eeee909446bd7
scyllacore <yms8795@gmail.com>
2022-11-30 23:53:44 +0900

    feat : 진입부(main) 구현.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.GameSystem;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameSystem gameSystem = new GameSystem();
+        gameSystem.runGame();
     }
 }
