0cc1c973911934f8d7fe30111df0de2ccaac6575
scyllacore <yms8795@gmail.com>
2022-11-30 23:20:53 +0900

    feat : 필요한 상수들을 저장하기 위한 클래스 구현.

---
+++ src/main/java/lotto/constants/LottoConstant.java
@@ -0,0 +1,12 @@
+package lotto.constants;
+
+public class LottoConstant {
+    public static final int LOTTO_PRICE = 1_000;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int NUMBERS_SIZE = 6;
+    public static final int MAX_AMOUNT_PAID = 100_000;
+    public static final int DIVISION_SIZE = 6;
+
+
+}
