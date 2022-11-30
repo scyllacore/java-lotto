c3c088a10cb7967bb99d430c66e76eb13d7e6a91
scyllacore <yms8795@gmail.com>
2022-11-30 23:25:39 +0900

    feat : 입력받은 금액에 대해 유효성 검사를 하기 위한 클래스 구현.

---
+++ src/main/java/lotto/view/valueobject/AmountPaid.java
@@ -0,0 +1,27 @@
+package lotto.view.valueobject;
+
+import lotto.exception.IllegalArgument;
+
+import java.util.regex.Pattern;
+
+public class AmountPaid {
+    private final Pattern amountPaidPattern = Pattern.compile("^\\d{4,6}$");
+
+    private final String amountPaid;
+
+    public AmountPaid(String amountPaid) {
+        validateAmountPaid(amountPaid);
+        this.amountPaid = amountPaid;
+    }
+
+    private void validateAmountPaid(String amountPaid){
+        if(amountPaidPattern.matcher(amountPaid).matches()){
+            return;
+        }
+        IllegalArgument.handleException(IllegalArgument.WRONG_INPUT.getMessage());
+    }
+
+    public String getAmountPaid() {
+        return amountPaid;
+    }
+}
